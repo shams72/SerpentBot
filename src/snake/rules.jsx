47d6f3dd71c2ed55d7b2ef4adc25a86302c8762d
Shams <shams.kabir@stud.h-da.de>
2025-05-23 16:34:04 +0200

    center snake

--- src/snake/rules.jsx
+++ src/snake/rules.jsx
@@ -5,7 +5,7 @@ const Rules = ({showRules,setShowRules}) => {
     <div
       style={{
         position: 'fixed',
-        top: '38.4%',
+        top: '37.4%',
         left: '50%',
         transform: 'translate(-50%, -30%)',
         backgroundColor: '#121212',
@@ -44,9 +44,9 @@ const Rules = ({showRules,setShowRules}) => {
         <p>
             The SerpentBot utilizes a Breadth-First Search (BFS) algorithm enhanced with the Manhattan distance heuristic to efficiently locate the nearest prey without crossing its own body. If no valid moves remain, it will halt to avoid collision.            <br /><br />
             🔴 You can generate prey by <strong>"Clicking"</strong> anywhere in the <strong>"Play Area"</strong> or by using the <strong>"Add 10 Prey"</strong> button.            <br /><br />
-            🤖 Click <strong>"Serpent Sense"</strong> to let the bot automatically navigate to the closest prey. Even while it's moving, you can add new prey — the bot will dynamically recalculate its path.
+            👽 Click <strong>"Serpent Sense"</strong> to let the bot automatically navigate to the closest prey. Even while it's moving, you can add new prey — the bot will dynamically recalculate its path.
             <br /><br />
-            🕹️ For full manual control, click <strong>"Keyboard Control"</strong>. You can use the arrow keys at any time. If <strong>"Serpent Sense"</strong> is active, you can still influence direction mid-path, but full control returns once it stops.
+            ⌨️ For full manual control, click <strong>"Keyboard Control"</strong>. You can use the arrow keys at any time. If <strong>"Serpent Sense"</strong> is active, you can still influence direction mid-path, but full control returns once it stops.
             <br /><br />
             🔁 You can switch between <strong>Keyboard Control</strong> and <strong>Serpent Sense</strong> at any time by clicking the corresponding button.
         </p>
