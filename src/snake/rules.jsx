9c76f353dfd0b0600c188237280747ff185361f8
Shams <shams.kabir@stud.h-da.de>
2025-05-23 17:27:51 +0200

    beautify

--- src/snake/rules.jsx
+++ src/snake/rules.jsx
@@ -42,15 +42,12 @@ const Rules = ({gridDimensions, showRules,setShowRules}) => {
             ✖️
         </button>
 
-        <p>
-            The SerpentBot utilizes a Breadth-First Search (BFS) algorithm enhanced with the Manhattan distance heuristic to efficiently locate the nearest prey without crossing its own body. If no valid moves remain, it will halt to avoid collision.            <br /><br />
-            🔴 You can generate prey by <strong>"Clicking"</strong> anywhere in the <strong>"Play Area"</strong> or by using the <strong>"Add 10 Prey"</strong> button.            <br /><br />
-            👽 Click <strong>"Serpent Sense"</strong> to let the bot automatically navigate to the closest prey. Even while it's moving, you can add new prey — the bot will dynamically recalculate its path.
-            <br /><br />
-            ⌨️ For full manual control, click <strong>"Keyboard Control"</strong>. You can use the arrow keys at any time. If <strong>"Serpent Sense"</strong> is active, you can still influence direction mid-path, but full control returns once it stops.
-            <br /><br />
-            🔁 You can switch between <strong>Keyboard Control</strong> and <strong>Serpent Sense</strong> at any time by clicking the corresponding button.
-        </p>
+        <p>The SerpentBot utilizes a Breadth-First Search (BFS) algorithm enhanced with the Manhattan distance heuristic to efficiently locate the nearest prey without crossing its own body. If no valid moves remain, it will halt to avoid collision.</p>
+            
+        <p>🔴 hah You can generate prey by <strong>"Clicking"</strong> anywhere in the <strong>"Play Area"</strong> or by using the <strong>"Add 10 Prey"</strong> button.</p> 
+        <p>👽 Click <strong>"Serpent Sense"</strong> to let the bot automatically navigate to the closest prey. Even while it's moving, you can add new prey — the bot will dynamically recalculate its path.</p>
+        <p>⌨️ For full manual control, click <strong>"Keyboard Control"</strong>. You can use the arrow keys at any time. If <strong>"Serpent Sense"</strong> is active, you can still influence direction mid-path, but full control returns once it stops.</p>
+        <p>🔁 You can switch between <strong>Keyboard Control</strong> and <strong>Serpent Sense</strong> at any time by clicking the corresponding button.   </p>
 
     </div>
   );
